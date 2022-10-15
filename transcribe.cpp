2957770791a6e7231be59a2a47047fb0dc067400
Vedat Ozkan <vedatkemalozkan@gmail.com>
2022-10-15 11:03:13 -0400

    a

--- transcribe.cpp
+++ transcribe.cpp
@@ -10,13 +10,13 @@ int main (int argc, char *argv[]) {
   string key = "Bb";
 
   string word;
-
+  cout << argc << endl;
   while (cin >> word) {
     cout << word << endl;
-    // if ('A' <= word[0] && word[0] <= 'G') {
-    //   Key key = keys.find(word.substr(0, 1));
-    //   key = keys.transpose(key, 2);
-    //   cout << key.key << endl;
-    // }
+    if ('A' <= word[0] && word[0] <= 'G') {
+      Key key = keys.find(word.substr(0, 1));
+      key = keys.transpose(key, 2);
+      cout << key.key << endl;
+    }
   }
 }
